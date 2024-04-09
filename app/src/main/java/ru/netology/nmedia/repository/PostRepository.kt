d8f3109f70a241d93f62803a5c5dddb87ddf6186
Дмитрий <94631730+symonf80@users.noreply.github.com>
2024-04-09 19:22:02 +0300

    Update PostRepository.kt

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -5,7 +5,7 @@ import ru.netology.nmedia.dto.Post
 interface PostRepository {
 
     fun saveAsync(post: Post, callback: GetResultCallback<Post>)
-    fun removeByIdAsync(id: Long, callback: GetResultCallback<Post>)
+    fun removeByIdAsync(id: Long, callback: GetResultCallback<Unit?>)
     fun getAllAsync(callback: GetResultCallback<List<Post>>)
     fun likeByIdAsync(post: Post, callback: GetResultCallback<Post>)
 
